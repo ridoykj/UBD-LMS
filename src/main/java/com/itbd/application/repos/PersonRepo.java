ae1495af1e4172d1f961d566292e338d6380ff81
ridoy.kumar <ridoy.kumar@gmail.com>
2024-01-07 15:37:08 +0600

    Delete unused repository interfaces and DAO classes

--- src/main/java/com/itbd/application/repos/PersonRepo.java
+++
@@ -1,9 +0,0 @@
-package com.itbd.application.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-import com.itbd.application.dao.user.person.PersonDAO;
-
-public interface PersonRepo extends JpaRepository<PersonDAO, Long>, JpaSpecificationExecutor<PersonDAO> {
-}
