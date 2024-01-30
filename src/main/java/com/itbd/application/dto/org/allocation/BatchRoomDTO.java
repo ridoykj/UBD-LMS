0deee38e2a930d4f27cf270a203db5c7c5ce8ada
ridoy.kumar <ridoykj@gmail.com>
2024-01-30 18:48:51 +0600

    Add EventTypeEnum and update BatchRoomDTO structure

--- src/main/java/com/itbd/application/dto/org/allocation/BatchRoomDTO.java
+++ src/main/java/com/itbd/application/dto/org/allocation/BatchRoomDTO.java
@@ -1,34 +1,56 @@
 package com.itbd.application.dto.org.allocation;
 
 import com.itbd.application.constants.DayTypeEnum;
+import com.itbd.application.constants.EventTypeEnum;
 import com.itbd.application.dao.org.allocation.BatchCourseDAO;
 import com.itbd.application.dao.org.allocation.BatchRoomDAO;
+import com.itbd.application.dao.org.place.BuildingDAO;
+import com.itbd.application.dao.org.place.FloorDAO;
 import com.itbd.application.dao.org.place.RoomDAO;
-import org.springframework.data.annotation.Version;
+import com.itbd.application.dao.org.place.SectorDAO;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public record BatchRoomDTO(
-        Long id,
+        @Id Long id,
         @Version Long version,
         DayTypeEnum dayName,
+        EventTypeEnum eventType,
         LocalDate startDate,
         LocalDate endDate,
         LocalTime startTime,
         LocalTime endTime,
         String description,
-        BatchCourseDAO batchCourse,
-        RoomDAO room
+        @NotNull BatchCourseDAO batchCourse,
+        @NotNull RoomDAO room
 ) {
 
 
     public static BatchRoomDTO fromEntity(BatchRoomDAO batchRoom) {
+        RoomDAO room = batchRoom.getRoom();
+        room.setReservations(null);
+        FloorDAO floor = room.getFloor();
+        floor.setRooms(null);
+        BuildingDAO building = floor.getBuilding() != null ? floor.getBuilding() : new BuildingDAO();
+        building.setFloors(null);
+        SectorDAO sector = building.getSector() != null ? building.getSector() : new SectorDAO();
+        sector.setBuildings(null);
 
+        building.setSector(sector);
+        floor.setBuilding(building);
+        room.setFloor(floor);
+        room.setBatchRooms(null);
+
+        batchRoom.setRoom(room);
         return new BatchRoomDTO(
                 batchRoom.getId(),
                 batchRoom.getVersion(),
                 batchRoom.getDayName(),
+                batchRoom.getEventType(),
                 batchRoom.getStartDate(),
                 batchRoom.getEndDate(),
                 batchRoom.getStartTime(),
@@ -43,6 +65,7 @@ public record BatchRoomDTO(
         batchRoom.setId(value.id());
         batchRoom.setVersion(value.version());
         batchRoom.setDayName(value.dayName());
+        batchRoom.setEventType(value.eventType());
         batchRoom.setStartDate(value.startDate());
         batchRoom.setEndDate(value.endDate());
         batchRoom.setStartTime(value.startTime());
