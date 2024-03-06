d50910044434b5e8a1ff48a310b9234ad9a04bf8
ridoy.kumar <ridoy.kumar@gmail.com>
2024-03-06 18:00:16 +0600

    Refactor code to improve performance and readability

--- frontend/views/org/edu/ProgrammeView.tsx
+++ frontend/views/org/edu/ProgrammeView.tsx
@@ -1,15 +1,12 @@
-import { Button } from "@hilla/react-components/Button.js";
 import { ComboBox, ComboBoxDataProviderCallback, ComboBoxDataProviderParams } from "@hilla/react-components/ComboBox.js";
-import { ConfirmDialog } from '@hilla/react-components/ConfirmDialog.js';
 import { FormLayout } from "@hilla/react-components/FormLayout.js";
-import { Icon } from '@hilla/react-components/Icon.js';
 import { Select } from '@hilla/react-components/Select.js';
-import { SplitLayout } from '@hilla/react-components/SplitLayout.js';
 import { TextField } from '@hilla/react-components/TextField.js';
-import { VerticalLayout } from "@hilla/react-components/VerticalLayout";
 import { useForm } from "@hilla/react-form";
 import BranchRC, { BranchCombobox } from "Frontend/components/branch/BranchRC";
 import { AutoGrid, AutoGridRef } from "Frontend/components/grid/autogrid";
+import SideCrudRC from "Frontend/components/layout/splitlayout/SideCrudRC";
+import SpeedDialRC from "Frontend/components/speeddial/SpeedDialRC";
 import ProgrammeTypeEnum from "Frontend/generated/com/itbd/application/constants/enums/ProgrammeTypeEnum";
 import DepartmentDTOModel from "Frontend/generated/com/itbd/application/dto/org/edu/DepartmentDTOModel";
 import ProgrammeDTO from "Frontend/generated/com/itbd/application/dto/org/edu/ProgrammeDTO";
@@ -17,9 +14,14 @@ import ProgrammeDTOModel from "Frontend/generated/com/itbd/application/dto/org/e
 import PropertyStringFilter from "Frontend/generated/dev/hilla/crud/filter/PropertyStringFilter";
 import Matcher from "Frontend/generated/dev/hilla/crud/filter/PropertyStringFilter/Matcher";
 import { DepartmentDtoCrudService, ProgrammeDtoCrudService } from "Frontend/generated/endpoints";
-import NotificationUtil from "Frontend/util/NotificationUtil";
 import { comboBoxLazyFilter } from "Frontend/util/comboboxLazyFilterUtil";
 import React, { useMemo, useState } from "react";
+import { FaUserPlus } from "react-icons/fa";
+
+const responsiveSteps = [
+  { minWidth: '0', columns: 1 },
+  { minWidth: '600px', columns: 2 },
+];
 
 const ProgrammeView = () => {
   const [branchFilter, setBranchFilter] = useState<BranchCombobox>({
@@ -27,31 +29,26 @@ const ProgrammeView = () => {
     departmentFilter: undefined,
   });
 
-
-  // const [orgFilter, setOrgFilter] = useState<OrganizationDAO>({} as OrganizationDAO);
-  // const [departmentFilter, setDepartmentFilter] = useState<DepartmentDAO>({} as DepartmentDAO);
-
-  const [dialogOpened, setDialogOpened] = useState<boolean>(false);
-  const [successNotification, setSuccessNotification] = useState<boolean>(false);
-
+  const [showSidebar, setShowSidebar] = useState<boolean>(false);
   const autoGridRef = React.useRef<AutoGridRef>(null);
-
   const [selectedProgrammeItems, setSelectedProgrammeItems] = useState<ProgrammeDTO[]>([]);
 
-  const { model, field, value, read, submit, clear, reset, visited, dirty, invalid, submitting } = useForm(ProgrammeDTOModel, {
+  const form = useForm(ProgrammeDTOModel, {
     onSubmit: async (programme) => {
       await ProgrammeDtoCrudService.save(programme).then((result) => {
         refreshGrid();
         setSelectedProgrammeItems(result ? [result] : []);
-        setSuccessNotification(true);
         clear();
       });
     }
   });
 
+  const { model, field, value, read, clear, reset } = form;
+
   function refreshGrid() {
     autoGridRef.current?.refresh();
   }
+
   const studyLevels = Object.values(ProgrammeTypeEnum).map(level => ({ label: level, value: level }));
 
   const departmentDataProvider = useMemo(
@@ -81,139 +78,82 @@ const ProgrammeView = () => {
     [branchFilter.organizationFilter]
   );
 
-  const responsiveSteps = [
-    { minWidth: '0', columns: 1 },
-    { minWidth: '600px', columns: 2 },
-  ];
+  const primary = () => {
+    return (
+      <>
+        <BranchRC
+          visibleFields={
+            { organization: true, department: true }
+          }
+          branchProps={{ branch: branchFilter, setBranch: setBranchFilter }}
+        />
+        <AutoGrid service={ProgrammeDtoCrudService} model={ProgrammeDTOModel} ref={autoGridRef}
+          visibleColumns={['name', 'code', 'studyLevel', 'department.name', 'status',]}
+          selectedItems={selectedProgrammeItems}
+          theme="row-stripes"  
+          columnOptions={{
+            'department.name': {
+              header: 'Department',
+              externalValue: branchFilter.departmentFilter != null ? branchFilter.departmentFilter.name : '',
+              // setExternalValue: setDepartmentFilter,
+            },
+          }}
+          onActiveItemChanged={(e) => {
+            const item = e.detail.value;
+            setSelectedProgrammeItems(item ? [item] : []);
+            read(item);
+            setShowSidebar(item?.id !== undefined);
+          }}        
+        />
+         <SpeedDialRC children={[
+          {
+            name: 'Add',
+            icon: <FaUserPlus />,
+            onClick: () => {
+              clear();
+              setSelectedProgrammeItems([]);
+              setShowSidebar(true);
+            }
+          },
+        ]} />
+      </>
+    );
+  }
 
-  const discardButtonColors: { [key: string]: string } = {
-    true: 'text-white bg-indigo-400 hover:bg-indigo-500',
-    false: 'text-white bg-gray-300',
-  };
+  const secondary = () => {
+    return (
+      <>
+        <FormLayout responsiveSteps={responsiveSteps} className="w-fit h-fit mx-5">
+          <label slot="label">Profile</label>
+          <ComboBox label={'Department'}  {...field(model.department)} dataProvider={departmentDataProvider} itemLabelPath='name' itemValuePath='name' clearButtonVisible />
+          <TextField label={'Name'}  {...field(model.name)} />
+          <Select label={'Study Level'}  {...field(model.studyLevel)} items={studyLevels} />
+          <TextField label={'Code'}  {...field(model.code)} />
+          <TextField label={'Description'}  {...field(model.description)} />
+          <TextField label={'Status'}  {...field(model.status)} />
+        </FormLayout>
+      </>
+    );
+  }
 
-  const saveButtonColors: { [key: string]: string } = {
-    true: 'text-white bg-emerald-400 hover:bg-emerald-500',
-    false: 'text-white bg-gray-300',
-  };
+  async function onConfirm() {
+    return await ProgrammeDtoCrudService.delete(selectedProgrammeItems[0]?.id).then((result) => {
+      refreshGrid();
+      setSelectedProgrammeItems([]);
+      reset();
+    });
+  }
 
   return (
     <>
-      <SplitLayout className="h-full w-full">
-        <VerticalLayout className="h-full w-full items-stretch">
-          <BranchRC
-            visibleFields={
-              { organization: true, department: true }
-            }
-            branchProps={{ branch: branchFilter, setBranch: setBranchFilter }}
-          />
-          <AutoGrid service={ProgrammeDtoCrudService} model={ProgrammeDTOModel} ref={autoGridRef}
-            visibleColumns={['name', 'code', 'studyLevel', 'department.name', 'status',]}
-            selectedItems={selectedProgrammeItems}
-            theme="row-stripes"
-            onActiveItemChanged={(e) => {
-              const item = e.detail.value;
-              setSelectedProgrammeItems(item ? [item] : []);
-              read(item);
-            }}
-            columnOptions={{
-              'department.name': {
-                header: 'Department',
-                externalValue: branchFilter.departmentFilter != null ? branchFilter.departmentFilter.name : '',
-                // setExternalValue: setDepartmentFilter,
-              },
-            }}
-          />
-        </VerticalLayout>
-        <VerticalLayout className="w-1/4 min-w-96">
-          <header className="bg-gray-100 w-full">
-            <div className="flex flex-row space-x-4">
-              <p className="text-blue-600 text-xl font-bold truncate p-1 m-1 w-full"># {value.name ?? 'Unknown Title'}</p>
-              <Button className="text-white content-end bg-blue-500 hover:bg-blue-600" onClick={clear}>
-                <Icon icon="vaadin:plus" />New
-              </Button>
-            </div>
-          </header>
-          <main className="overflow-y-scroll w-full h-full">
-            <FormLayout responsiveSteps={responsiveSteps} className="w-fit h-fit mx-5">
-              <label slot="label">Profile</label>
-              <ComboBox label={'Department'}  {...field(model.department)} dataProvider={departmentDataProvider} itemLabelPath='name' itemValuePath='name' clearButtonVisible />
-              <TextField label={'Name'}  {...field(model.name)} />
-              <Select label={'Study Level'}  {...field(model.studyLevel)} items={studyLevels} />
-              <TextField label={'Code'}  {...field(model.code)} />
-              <TextField label={'Description'}  {...field(model.description)} />
-              <TextField label={'Status'}  {...field(model.status)} />
-            </FormLayout>
-          </main>
-          <footer className="flex flex-row bg-gray-100 w-full">
-            <div className="w-full">
-              {
-                selectedProgrammeItems[0]?.id === undefined ? null :
-                  <Button
-                    className="text-white bg-red-400 hover:bg-red-500"
-                    onClick={() => {
-                      setDialogOpened(true);
-                      console.log('delete', selectedProgrammeItems[0]?.id);
-                    }}
-                  >Delete</Button>
-              }
-            </div>
-            {
-              value.name === undefined ? null :
-                <div className="flex flex-row content-end space-x-4">
-                  <Button
-                    className={discardButtonColors[dirty.toString()]}
-                    disabled={!dirty}
-                    onClick={reset}
-                  >
-                    Discard
-                  </Button>
-                  <Button
-                    className={saveButtonColors[dirty.toString()]}
-                    disabled={invalid || submitting || !dirty}
-                    onClick={submit}
-                  >
-                    {selectedProgrammeItems[0]?.id !== undefined ? 'Update' : 'Save'}
-                  </Button>
-                </div>
-            }
-          </footer>
-        </VerticalLayout>
-      </SplitLayout>
-      <NotificationUtil opened={successNotification} type="update"
-        message={{
-          title: 'Successfully Updated',
-          description: value.name,
-        }}
-        onOpenedChanged={(event) => {
-          if (!event.detail.value) {
-            setSuccessNotification(event.detail.value);
-          }
-        }}
-        onClick={() => { setSuccessNotification(false) }}
-      />
-      <ConfirmDialog
-        header="Delete Item"
-        cancelButtonVisible
-        rejectButtonVisible
-        rejectText="Discard"
-        confirmText="Confirm"
-        confirmTheme="error primary"
-        opened={dialogOpened}
-        onOpenedChanged={(event) => {
-          setDialogOpened(event.detail.value);
-          if (event.detail.value) {
-            // setStatus('');
-          }
-        }}
-        onConfirm={() => {
-          ProgrammeDtoCrudService.delete(selectedProgrammeItems[0]?.id).then((result) => {
-            refreshGrid();
-            setSelectedProgrammeItems([]);
-            reset();
-          });
-        }}>
-        {`Do you want to delete?${value.name}`}
-      </ConfirmDialog >
+      <SideCrudRC
+        primary={primary()}
+        secondary={secondary()}
+        form={form}
+        onConfirm={onConfirm}
+        showSidebar={showSidebar}
+        setShowSidebar={setShowSidebar}
+      />      
     </>
   );
 };
