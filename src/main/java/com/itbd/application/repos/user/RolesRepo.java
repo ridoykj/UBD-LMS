a7fa5f0c60634b7bd5d7d078ee9ab3309258cf31
ridoy.kumar <ridoy.kumar@gmail.com>
2024-03-10 07:35:52 +0600

    Delete RolesDAO, RoleDTO, and RolesRepo

--- src/main/java/com/itbd/application/repos/user/RolesRepo.java
+++
@@ -1,8 +0,0 @@
-package com.itbd.application.repos.user;
-
-import com.itbd.application.dao.user.RolesDAO;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface RolesRepo extends JpaRepository<RolesDAO, Long>, JpaSpecificationExecutor<RolesDAO> {
-}
