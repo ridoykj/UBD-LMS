3f8ce2f8f284febb75f83064b347709d40ea9672
ridoy.kumar <ridoy.kumar@gmail.com>
2024-03-10 13:54:58 +0600

    Fix error handling in ImgRC component

--- frontend/components/Image/ImgRC.tsx
+++ frontend/components/Image/ImgRC.tsx
@@ -1,7 +1,11 @@
+import { ReactEventHandler } from "react";
 
 const ImgRC = ({ imageUrl }: { imageUrl: string }) => {
-    const handleError = (e: any) => {
-        e.target.src = `images/default/no_image.png`;
+    const handleError: ReactEventHandler<HTMLImageElement> = (e) => {
+        if (e.target instanceof HTMLImageElement) {
+            e.target.onerror = null;
+            e.target.src = `images/default/no_image.png`;
+        }
     };
     return (
         <img className="w-8 h-8 shrink-0 rounded-full ring-2 ring-blue-500 object-cover bg-no-repeat" src={imageUrl} alt="no_image" onError={handleError} />
