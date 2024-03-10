88d5a3a3421f2f575aaf54249b19715a2cc08bf5
ridoy.kumar <ridoy.kumar@gmail.com>
2024-03-10 15:35:08 +0600

    Remove unused imports in WeApplication.java

--- src/main/java/com/itbd/application/WeApplication.java
+++ src/main/java/com/itbd/application/WeApplication.java
@@ -5,8 +5,6 @@ import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * The entry point of the Spring Boot application.
