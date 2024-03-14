d9ce4a6cc7362f94db520f3031cc45b2460ebf12
ridoy.kumar <ridoy.kumar@gmail.com>
2024-03-14 08:42:26 +0600

    Add AppAccess annotation

---
+++ src/main/java/com/itbd/application/annotations/AppAccess.java
@@ -0,0 +1,14 @@
+package com.itbd.application.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD})
+public @interface AppAccess {
+    String action();
+    String name();
+    String description() default "";
+}
