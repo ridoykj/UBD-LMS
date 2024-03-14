5c985bf9443a21c2ab1f31377ae6cdaf51c80f12
ridoy.kumar <ridoy.kumar@gmail.com>
2024-03-14 08:46:19 +0600

    Add RoleDto class with mapping methods

---
+++ src/main/java/com/itbd/application/dto/resources/RoleDto.java
@@ -0,0 +1,49 @@
+package com.itbd.application.dto.resources;
+
+import com.itbd.application.dao.resources.RolesDao;
+import com.itbd.application.dao.resources.permissions.AppPermissionDao;
+import jakarta.persistence.Id;
+import org.springframework.data.annotation.Version;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public record RoleDto(
+        @Id Long id,
+        @Version Long version,
+        String name,
+        String description,
+        Set<AppPermissionDao> permissions) {
+    public static RoleDto fromEntity(RolesDao roles) {
+        roles.setUsers(null);
+//        roles.setPermissions(null);
+        return new RoleDto(
+                roles.getId(),
+                roles.getVersion(),
+                roles.getName(),
+                roles.getDescription(),
+                roles.getPermissions());
+    }
+
+    public static void fromDTO(RoleDto roleDTO, RolesDao rolesDAO) {
+        rolesDAO.setId(roleDTO.id());
+        rolesDAO.setVersion(roleDTO.version());
+        rolesDAO.setName(roleDTO.name());
+        rolesDAO.setDescription(roleDTO.description());
+//        rolesDAO.setPermissions(roleDTO.permissions());
+
+//        Set<AppPermissionDao> permissions = roleDTO.permissions().stream().map(appPermissionDao -> {
+//            appPermissionDao.setRole(rolesDAO);
+//            appPermissionDao.setResource(appPermissionDao.getResource());
+//            return appPermissionDao;
+//        }).collect(Collectors.toSet());
+
+//        Set<AppPermissionDao> permissions = roleDTO.permissions().stream().map(appPermissionDao -> {
+////            appPermissionDao.setRole(rolesDAO);  // modify the role property here
+//            return appPermissionDao;
+//        }).collect(Collectors.toSet());
+//        rolesDAO.setPermissions(permissions);
+        rolesDAO.setPermissions(roleDTO.permissions());
+//        rolesDAO.setPermissions(roleDTO.permissions());
+    }
+}
