babcc5679de5bae6ce3acf515abe4eb45931e3fe
ridoy.kumar <ridoy.kumar@gmail.com>
2024-03-15 09:27:27 +0600

    Add resource and action enums to AppAccess annotation

--- src/main/java/com/itbd/application/annotations/AppAccess.java
+++ src/main/java/com/itbd/application/annotations/AppAccess.java
@@ -1,5 +1,8 @@
 package com.itbd.application.annotations;
 
+import com.itbd.application.constants.enums.appresource.AppPermissionsEnum;
+import com.itbd.application.constants.enums.appresource.AppResourcesEnum;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -8,7 +11,8 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD})
 public @interface AppAccess {
-    String action();
+    AppResourcesEnum resource();
+    AppPermissionsEnum action();
     String name();
     String description() default "";
 }
